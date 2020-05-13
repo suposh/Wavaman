04f688731b0d19921b8e201e401c5f1ef380483f
Suposh Mehta <suposhmehta@yahoo.in>
2020-05-14 02:22:22 +0530

    Got static error fixed. Updated sdkconfig for 4MB Esp storage.

--- main/main.c
+++ main/main.c
@@ -28,6 +28,7 @@ static void event_handler(lv_obj_t * obj, lv_event_t event);
 
 void guiTask();
 void event_handler(lv_obj_t * obj, lv_event_t event);
+void menu(lv_obj_t *tabview, lv_obj_t *tab1, lv_obj_t *tab2);
 signed int id = 0;
 signed int dir = -1;
 /**********************
@@ -61,6 +62,8 @@ void guiTask() {
     static lv_color_t buf2[DISP_BUF_SIZE];
     static lv_disp_buf_t disp_buf;
 
+	static lv_obj_t *tabview, *tab1, *tab2;
+
     lv_disp_buf_init(&disp_buf, buf1, buf2, DISP_BUF_SIZE);
 
     lv_disp_drv_t disp_drv;
@@ -87,133 +90,136 @@ void guiTask() {
 
     /* use a pretty small demo for 128x64 monochrome displays */
     lv_obj_t * scr = lv_disp_get_scr_act(NULL);     /*Get the current screen*/
+
+
 	/*Create a Tab view object*/
-    lv_obj_t *tabview;
+	tabview = lv_tabview_create(lv_scr_act(), NULL);
+	/*Add 2 tabs (the tabs are page (lv_page) and can be scrolled*/
+	tab1 = lv_tabview_add_tab(tabview, "SET");
+	tab2 = lv_tabview_add_tab(tabview, "STATS");
 
-    tabview = lv_tabview_create(lv_scr_act(), NULL);
-	lv_style_t style1 = lv_style_transp, style2 = lv_style_plain_color, style3 = lv_style_transp;
-	lv_tabview_set_anim_time(tabview, 200);
-    /*Add 2 tabs (the tabs are page (lv_page) and can be scrolled*/
-    lv_obj_t *tab1 = lv_tabview_add_tab(tabview, "SET");
-
-	lv_style_t style7 = lv_style_pretty_color; //lv_style_pretty
-	style7.body.opa = LV_OPA_TRANSP;
-	style7.body.border.width = 0;
-	lv_page_set_style(tab1, LV_PAGE_STYLE_SB, &style7);		// PAGE Scroll bar hidden
-
-    lv_obj_t *tab2 = lv_tabview_add_tab(tabview, "STATS");
-
-
+	lv_style_t style1 = lv_style_transp;
 	style1.body.padding.top = 1;
 	style1.body.padding.bottom = 1;
 	lv_tabview_set_style(tabview, LV_TABVIEW_STYLE_BTN_BG,  &style1);
 
+	lv_style_t style2 = lv_style_plain_color;
 	style2.body.padding.inner = 0;
 	lv_tabview_set_style(tabview, LV_TABVIEW_STYLE_INDIC,  &style2);
 
+	lv_style_t style3 = lv_style_transp;
 	style3.body.padding.top = 3;
 	style3.body.padding.bottom = 1;
 	lv_tabview_set_style(tabview, LV_TABVIEW_STYLE_BTN_REL,  &style3);
 	lv_obj_refresh_style(tabview);
 
-    /*Create a list*/
-    lv_obj_t * list1 = lv_list_create(tab1, NULL);
-    lv_obj_set_size(list1, 128, 50);
-    lv_obj_align(list1, NULL,  LV_ALIGN_IN_TOP_LEFT, 0, 0);
-	lv_list_set_anim_time(list1, 60);
-	lv_list_set_sb_mode(list1, LV_SB_MODE_OFF); 	// LIST Disable Scroll Bar
-	// lv_list_set_layout(list1, LV_LAYOUT_COL_L);
-
-	lv_style_t style4 = lv_style_transp_fit;
-	style4.body.border.width =0;
-	style4.body.padding.left = -1;
-	style4.body.padding.right = -1;
-	// style4.body.padding.top = 0;
-	lv_list_set_style(list1, LV_LIST_STYLE_BG, &style4);
-
-	lv_style_t style5 = lv_style_transp_fit; //lv_style_pretty
-	style5.body.padding.inner = -3;
-	style5.body.border.width = 0;
-	// style5.body.padding.top = -5;
-	lv_list_set_style(list1, LV_LIST_STYLE_SCRL, &style5);
-
-    lv_obj_t * list_btn[5];
-	// To adjust the height of an individual list button,
-	// dimensions of the container need to be changed.
-	lv_style_t style6 = lv_style_pretty;
-	style6.body.padding.top = 6;
-	style6.body.padding.bottom = 5;
-	style6.body.radius = 0;
-
-	// Long Strings cause glitchy scroll, in the button label.
-    list_btn[0] = lv_list_add_btn(list1, NULL, "1.Frequency");
-	lv_obj_set_event_cb(list_btn[0], event_handler);
-	lv_btn_set_style(list_btn[0],LV_CONT_STYLE_MAIN, &style6);
-
-    list_btn[1] = lv_list_add_btn(list1, NULL, "2.Amplitude");
-    lv_obj_set_event_cb(list_btn[1], event_handler);
-	lv_btn_set_style(list_btn[1],LV_CONT_STYLE_MAIN, &style6);
-
-    list_btn[2] = lv_list_add_btn(list1, NULL, "3.Waveform");
-    lv_obj_set_event_cb(list_btn[2], event_handler);
-	lv_btn_set_style(list_btn[2],LV_CONT_STYLE_MAIN, &style6);
-
-	list_btn[3] = lv_list_add_btn(list1, NULL, "4.Logic In");
-    lv_obj_set_event_cb(list_btn[3], event_handler);
-	lv_btn_set_style(list_btn[3],LV_CONT_STYLE_MAIN, &style6);
-
-	list_btn[4] = lv_list_add_btn(list1, NULL, "5.Stats");
-    lv_obj_set_event_cb(list_btn[4], event_handler);
-	lv_btn_set_style(list_btn[4],LV_CONT_STYLE_MAIN, &style6);
+	lv_tabview_set_anim_time(tabview, 200);
+	lv_style_t style7 = lv_style_pretty_color; //lv_style_pretty
+	style7.body.opa = LV_OPA_TRANSP;
+	style7.body.border.width = 0;
+	lv_page_set_style(tab1, LV_PAGE_STYLE_SB, &style7);		// PAGE Scroll bar hidden
 
 
-	struct ls{
-		lv_obj_t *btn[5];
-		lv_obj_t *list;
-	} *obj1, obj;
-	// ls obj;
-	for(uint8_t i=0; i<5; i++) obj.btn[i] = list_btn[i];
-	obj.list = list1;
-	obj1 = &obj;
+	menu(tabview, tab1, tab2);
 
-	void pseudo_button(lv_task_t *task){
-		struct ls *obj;
-		obj = task->user_data;
-		lv_obj_t *btn[5];
-		for(uint8_t i=0; i<5; i++) btn[i]= obj->btn[i];
-		lv_obj_t *list = obj->list;
-
-
-
-		// lv_list_down(list1);
-
-		if(dir == 1){		// go down
-			// lv_list_down(list);
-			printf("id %d\t dir%d\n",id, dir);
-			// lv_list_focus(btn[id], LV_ANIM_ON);
-			lv_list_set_btn_selected(list, btn[id]);
-			if(id == 0) {
-				dir = -1;
-				id++;
-			}
-			else id--;
-		}
-		else{		// go up
-			// lv_list_up(list);
-			printf("id %d\t dir%d\n",id, dir);
-			// lv_list_focus(btn[id], LV_ANIM_ON);
-			lv_list_set_btn_selected(list, btn[id]);
-			if(id == 4){
-				dir = 1;
-				id--;
-			}
-			else id++;
-		}
-
-		// else id++;
-		// else printf("change\n");
-	}
-	lv_task_t *pseudo_button_task = lv_task_create(pseudo_button, 1000, LV_TASK_PRIO_MID, obj1);
+    // /*Create a list*/
+    // lv_obj_t * list1 = lv_list_create(tab1, NULL);
+    // lv_obj_set_size(list1, 128, 50);
+    // lv_obj_align(list1, NULL,  LV_ALIGN_IN_TOP_LEFT, 0, 0);
+	// lv_list_set_anim_time(list1, 60);
+	// lv_list_set_sb_mode(list1, LV_SB_MODE_OFF); 	// LIST Disable Scroll Bar
+	// // lv_list_set_layout(list1, LV_LAYOUT_COL_L);
+	//
+	// lv_style_t style4 = lv_style_transp_fit;
+	// style4.body.border.width =0;
+	// style4.body.padding.left = -1;
+	// style4.body.padding.right = -1;
+	// // style4.body.padding.top = 0;
+	// lv_list_set_style(list1, LV_LIST_STYLE_BG, &style4);
+	//
+	// lv_style_t style5 = lv_style_transp_fit; //lv_style_pretty
+	// style5.body.padding.inner = -3;
+	// style5.body.border.width = 0;
+	// // style5.body.padding.top = -5;
+	// lv_list_set_style(list1, LV_LIST_STYLE_SCRL, &style5);
+	//
+    // lv_obj_t * list_btn[5];
+	// // To adjust the height of an individual list button,
+	// // dimensions of the container need to be changed.
+	// lv_style_t style6 = lv_style_pretty;
+	// style6.body.padding.top = 6;
+	// style6.body.padding.bottom = 5;
+	// style6.body.radius = 0;
+	//
+	// // Long Strings cause glitchy scroll, in the button label.
+    // list_btn[0] = lv_list_add_btn(list1, NULL, "1.Frequency");
+	// lv_obj_set_event_cb(list_btn[0], event_handler);
+	// lv_btn_set_style(list_btn[0],LV_CONT_STYLE_MAIN, &style6);
+	//
+    // list_btn[1] = lv_list_add_btn(list1, NULL, "2.Amplitude");
+    // lv_obj_set_event_cb(list_btn[1], event_handler);
+	// lv_btn_set_style(list_btn[1],LV_CONT_STYLE_MAIN, &style6);
+	//
+    // list_btn[2] = lv_list_add_btn(list1, NULL, "3.Waveform");
+    // lv_obj_set_event_cb(list_btn[2], event_handler);
+	// lv_btn_set_style(list_btn[2],LV_CONT_STYLE_MAIN, &style6);
+	//
+	// list_btn[3] = lv_list_add_btn(list1, NULL, "4.Logic In");
+    // lv_obj_set_event_cb(list_btn[3], event_handler);
+	// lv_btn_set_style(list_btn[3],LV_CONT_STYLE_MAIN, &style6);
+	//
+	// list_btn[4] = lv_list_add_btn(list1, NULL, "5.Stats");
+    // lv_obj_set_event_cb(list_btn[4], event_handler);
+	// lv_btn_set_style(list_btn[4],LV_CONT_STYLE_MAIN, &style6);
+	//
+	//
+	// struct ls{
+	// 	lv_obj_t *btn[5];
+	// 	lv_obj_t *list;
+	// } *obj1, obj;
+	// // ls obj;
+	// for(uint8_t i=0; i<5; i++) obj.btn[i] = list_btn[i];
+	// obj.list = list1;
+	// obj1 = &obj;
+	//
+	// void pseudo_button(lv_task_t *task){
+	// 	struct ls *obj;
+	// 	obj = task->user_data;
+	// 	lv_obj_t *btn[5];
+	// 	for(uint8_t i=0; i<5; i++) btn[i]= obj->btn[i];
+	// 	lv_obj_t *list = obj->list;
+	//
+	//
+	//
+	// 	// lv_list_down(list1);
+	//
+	// 	if(dir == 1){		// go down
+	// 		// lv_list_down(list);
+	// 		printf("id %d\t dir%d\n",id, dir);
+	// 		// lv_list_focus(btn[id], LV_ANIM_ON);
+	// 		lv_list_set_btn_selected(list, btn[id]);
+	// 		if(id == 0) {
+	// 			dir = -1;
+	// 			id++;
+	// 		}
+	// 		else id--;
+	// 	}
+	// 	else{		// go up
+	// 		// lv_list_up(list);
+	// 		printf("id %d\t dir%d\n",id, dir);
+	// 		// lv_list_focus(btn[id], LV_ANIM_ON);
+	// 		lv_list_set_btn_selected(list, btn[id]);
+	// 		if(id == 4){
+	// 			dir = 1;
+	// 			id--;
+	// 		}
+	// 		else id++;
+	// 	}
+	//
+	// 	// else id++;
+	// 	// else printf("change\n");
+	// }
+	// lv_task_t *pseudo_button_task = lv_task_create(pseudo_button, 1000, LV_TASK_PRIO_MID, obj1);
 
 
 
@@ -237,4 +243,7 @@ static void event_handler(lv_obj_t * obj, lv_event_t event)
     if(event == LV_EVENT_CLICKED) {
         printf("Clicked: %s\n", lv_list_get_btn_text(obj));
     }
+}
+void menu(lv_obj_t *tabview, lv_obj_t *tab1, lv_obj_t *tab2) {
+	lv_obj_t *tab3 = lv_tabview_add_tab(tabview, "OO");
 }
